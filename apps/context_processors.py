6939d03ffe88f11241032b46b77ab34923c84d10
Kholmumin <xolmomin@gmail.com>
2025-11-07 15:59:19 +0500

    changes

---
+++ apps/context_processors.py
@@ -0,0 +1,4 @@
+def names(request):
+    return {
+        "name": "HelloPython"
+    }
--- root/settings.py
+++ root/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "apps.context_processors.names",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
